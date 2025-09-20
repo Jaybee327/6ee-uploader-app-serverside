770cd8b3801fe7151a96879a984e1d33f6453658
Jaybee327 <164274598+Jaybee327@users.noreply.github.com>
2025-09-20 05:40:08 -0700

    Update server.js

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const upload = multer();
 const app = express();
 const port = 3000;
 
+
 // 1. For App Key -------------------------
 const APP_KEY = '1vva13287tatkzs'; // create a new app key: Go to the Dropbox Developer Dashboard (https://www.dropbox.com/developers) and create a new app (call it whatever). This will give you access to the Dropbox API,
 // find and click Generate an access token for your app. This token will allow your app to access your Dropbox account via this app.
@@ -18,36 +19,14 @@ const APP_KEY = '1vva13287tatkzs'; // create a new app key: Go to the Dropbox De
 // 2. For App Secret -------------------------
 const APP_SECRET = 'j6xn07nel550jma'; // you'll also find app secret code in the dash board...
 
-// 3. For Access Token -------------------------
-let ACCESS_TOKEN = 'sl.u.AF86zc7DdsjwwePHwm1lS4t6UhPWwMkxCn-OQZPJcGN4_7Laqc-bqqtj69nZM0IACBot4WufuBuMGIxa7lW_J6DuUaFqIuN9AMH0VcxlnJ34F53l6fuEfeBQ0m9KDGiglG6ErR8-0MIqfMMihAXkSW62WHWm0S05n-ML6m5yt97cX3sm7-EoEVnAKJasyexs9VCtSf1XCIPuglZk4Hp67BkM_KvzZ5Mit-h1n9DLd5Kbgc8b0S7PkO6PWiop-uT7I2wOfXvDAcGzRgPVqU-EEr1vn_4tSj0CS5s55QlLIjnaIjsygqDVxbkcNmIhExIXwjpngi8L6CJn09Dhr3hn05EC7rSpCFUXVgwmZH251AAiRJz331Zzua80qYsgzs-85kHMZ3MIRsl4I81f9QYiZMAyfTFluDDK2u1LQODPvUd6pa560Awy09cZIsRkvdJ_tu217uinph2s62IHeos1dltZC_ggvUdmrsgFO2xb4PImR4HUyuojVeDoQbQMO9k-0UiGgoYutihKoruErXyPJ1HVGuJFVVvoja9l0cHk45ltV4De-amcJvFF5WtMmot_Yqqtf1xhVPhhYi8m7XKJhnmCvdLTe3q1GyKsMAWlFovHlPZVKvQUIkYZdX57sOw4sv3VoUgvVlGpk5QyqBsyhy4yv7tEHy9KI2t7Z6rzLKcFkkN51138zxInHUFJsYvf22xsosltiJlXUe8rnsVwVhtpy3FLhWfeDkCiRLUMH1D39Ecu9cA6x4TVeMhZPYwOEX_TwVkZotoigRM_iUDQI_hfT8aHHelzxVEEKpaGicknajQGj2F9jFegNLBvgtIJ1LvjYS1PtZ2Rafgo0vFpc3QjxGUkZOHi18PkBIMAGRc69XH_4bcqeZD9hBB-EiXoKgtdyqI-7Bli2839VyV2WKc45Du4nrN3VW8JBXAos8IB3Tl4JvB7GjpEFPdgIFrKi13jSbKPX2yExs_Wvu8u9zkjbsj0KLHxjWTDdmrXcnuWcvGmTDWweh8j4-CJ0U5BBRHww3cnouMgKpXPxY_1iXVo53vn6y2CWm-5LqtAcxTy8dzDp0MpnqR6hoL6XVBYzIr7r-uvAmI_DitCyqPFrokp6Atgl5zEKoRsvH9SdicQHljvXwbuklmNDs6u21tRNyx-RPf0KXqHiJIaNm-CjlR-IhRyPygp6RK0iVya8BKkV5ktaBkI_axrQzumEkr0XyCP6yDRVnlkNPIZoWjqjTKgotdgEIdJB9eWDBq51k4LwZBRbaLTRTmkXRkOl1RqHqzb_CFZ-nfmij9kWbVBCY41Wc9a0mefV_chUBNjV3p5eOyX-MfK-dFQS1HVzARMThCveNAHuIdfZaN2cdYAA8TXCQSosWKEs_On4lkdXaMFqXXM5TqqHjMOKE-TGcrbVisaQomyZ7Tc6XdLdstG_LCp';
-// Access token is extremely lenghty and can be found in....
-// get access token with this link on your browser (after creating a developer account with dropbox and also after getting Your Appkey)
-// https://www.dropbox.com/oauth2/authorize?response_type=code&client_id=YOUR_APP_KEY
+// 3. For Access Token and refresh token -------------------------
+// obtain authorise code first by pasting the link below on your browser after obtaining your app key...
+// https://www.dropbox.com/oauth2/authorize?client_id=1vva13287tatkzs&token_access_type=offline&response_type=code ---- this link gives u auth code
+// then paste auth code in between the follwing link below and paste in CMD to get access and refresh token
+// curl --location --request POST "https://api.dropboxapi.com/oauth2/token" --user "1vva13287tatkzs:j6xn07nel550jma" --header "Content-type: application/x-www-form-urlencoded" --data-urlencode "code=yZj9NDtoeToAAAAAAAAAIZmKq3FvMLqvTpsXJFpsQWQ" --data-urlencode "grant_type=authorization_code"
 
-// 4. For Temp Authoization code -------------------------
-// you will need this Authorization Code Link direct link in the process to get your main refresh token (to get)
-// AUTHORIZATION_CODE = "yZj9NDtoeToAAAAAAAAAHU1FTE09tRCI67Cy_anpRxY"; // This will only be used in the process of getting the Refresh Token
-// get this auth code... do this below.
-// paste this in your browser and click approve
-// https://www.dropbox.com/oauth2/authorize?client_id=pasteYourAppkeyHere&token_access_type=offline&response_type=code
-// Save it somewhere you will see where to use it below.
-
-// 5. For Main Refresh token -------------------------
-const REFRESH_TOKEN = 'rlFloC6oG80AAAAAAAAAAXDCpHJohUrVy5ceysy4o5SF0qHxcF3zof-k4DDL_B4X'; // (from CMD [process below]) It sould be in this format.
-
-// get refresh token with this in CMD (this token refreshes automatically forever) - choose your method below [which ever works or is simpler to you]
-
-// 1. first method (paste whats below in CMD). ---- dont use this method (skip)
-// curl -X POST \
-// https://api.dropboxapi.com/oauth2/token \
-// -d "grant_type=refresh_token&refresh_token=YOUR_REFRESH_TOKEN" \
-// -u YOUR_APP_KEY:YOUR_APP_SECRET
-
-
-// 2. best method (paste whats below in CMD).
-// curl --location --request POST "https://api.dropboxapi.com/oauth2/token" --user "YOUR_APP_KEY:YOUR_APP_SECRET" --header "Content-type: application/x-www-form-urlencoded" --data-urlencode "code=AUTHORIZATION_CODE" --data-urlencode "grant_type=authorization_code"
-
-	
+let ACCESS_TOKEN = 'sl.u.AF-1_CmHC5Uqhx-8bXYXToaZCtUTGTYhmdrUmv_t0kQrwN-kGJukE7BOZ8zyZ2bAQxMSTPU4Xacj0MHsHT7rbPWe1Y1OHEhQujvMAVGqrSYNQBsyvHD10EsS7XlfpXeR1R4xyRJ_nhE-UI0diOeZNAI4k1_REDkNgBjExQlyq0FVkiVuJNYFGyfdVW3C3whF02SZtz4raBo4An_UGy0OqEc3ZtcqWOy48zuOzZVQ_jsqERFcR669aNZUvQ3Jp58AvrRhDtufRkNB-Jl0OMg9KvF74mkeMPfuEvmkYJJpmlCoNxj9yGJJOaOZCNJ1WL2KTCVHgTjnWFmk_M4UcXr8AMKzJ2dgUfRTYixjPwF02wWB7yYZKGKhMetSSd7vvc3UfI6xTcox8eyOPn37G7BpcRIideuU6hP6QkVNaG0M3eCPnqg_JGHxBxw8TjxAhqsUPzJMot61fLPHXW2X52Zo7cZvfbNzKd0oSJzgM6ssbjDpOXagVAWFRgeQ-9lxrl9W0ouN8Qt72bLIHL5KxWmTQ4n9qFGLcJBi5pDc2e2LCigKeXXIAl9e8W3IF9bgtMKtU0GCIT6oPWIR1vQ3evc9yHmDm67FOU58_Le03RCumelRSZtmW447sXt-lFYO6n_IEQH3UF64cdtllZlm39JiD1Xb5eCbE-ildBoObqRdMmFvE8rUsdL3ItAhKxU3deJIyAQxQddUckY8xLEqnPeS6hFXULsww6Jyb8xtqJNms8KEL1oOYD7UgR6pZGdpmJxJHBQ4UaavVZbeFqG-iwkSgaljggvOrybCQ01K5o5EfR01xOVgxsUugAUe0k39Ftxfrrja2UKpHk4yMR60ayC1BQiFPDgddZ0-9EcM-cmQO4xzuF6iRPTjWv6IA9yvpO4IoDWMJHIA8pnFH66kMXbdbMPNpBvq0OBA-1FEVeDCCLM5NwcZ2klCruRCyxcZ9qbSQltbfOASD3sLYJfa89BvfKlB4JD-xBnFwH7lMSzuAF2s-ksjNKdp-rWw4hVFMqh1SFPKm5YX_mBlSc0hLFJduuPKkUHebNggSjqJ-66zZlpdgwjBZ4NkIiWCm1kz4kDfGBKwz882bVmURS_-OD2dmmiGSzDUkAorcgxdhvHWLWdqtrD21XpfFCo2juLIxrbWRqohoPgmjKjV0wfFqFLpJ3nUqI9tFvlJakhRaKs1AOBb9Tt9yCN0XAdhGgFGSL2yPklZmrDqRLXYZZpRIawsyZyqWXzD-pFDm3q9lORBA9edQRTM-Pcd_XIP9sDbneAnDSXON0bbdRhokASZy8Lhkuu2sfQ7hR1T6VMaltqiM96TZYNNFp_5hBft9isF6ZqvVKAyW9tGDtKrYml2HD4-UtWMjjxEOZLn4HUbHzsb1QuyhBNS92l7OyiAzQFWPdEX2O03x0uIwlfwnosqrn8kAUSt';
+const REFRESH_TOKEN = 'ao96GGSzLTIAAAAAAAAAAfsqryas4vx5eZi9Xo1dvO75JN1gsoi2488Xm7Tpbfdv';
 
 
 // dont edit anything below except an expert.
@@ -131,4 +110,5 @@ app.post('/upload-text', async (req, res) => {
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
 });
+
 
